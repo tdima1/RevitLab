566f5d7bafc85836879fad7df4c04fe011db6c93
Dima Tiberiu <dima.tiberiu1@gmail.com>
2019-07-03 13:44:07 +0300

    update v.2

---
+++ RevitLab/Lab7Utils.cs
@@ -0,0 +1,19 @@
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RevitLab
+{
+   public class Lab7Utils
+   {
+
+      ICommand _getCubeSize = new DelegateCommand((param) => Lab7Button.DrawCube(Convert.ToInt32(param)));
+
+      public ICommand GetCubeSize { get { return _getCubeSize; } }
+
+   }
+}
